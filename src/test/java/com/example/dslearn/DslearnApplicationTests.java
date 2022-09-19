70dda3af495b545376cbbcfcd139836817dc8f5d
bruno lessa <brunolessa@live.com>
2022-09-19 17:57:29 -0300

    Project created

---
+++ src/test/java/com/example/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
